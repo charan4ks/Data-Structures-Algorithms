aa2e7053ab8519cf33825bc01c0bc34f8cb969aa
Charan <saicharan@arth.team>
2022-07-07 23:11:32 +0530

    changes

--- reverse_words_in_a_string.cpp
+++ reverse_words_in_a_string.cpp
@@ -36,3 +36,48 @@ public:
         return ans;
     }
 };
+
+
+//--------------------------------Approach2--------------------------------
+//Using stack
+
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        int count =1;
+        string ans;
+        stack<string>p;
+        
+        string temp="";
+        for(int i=0; i<n; i++){
+            if(s[i] == ' ' && temp.length()>0){
+                p.push(temp);
+                count++;
+                temp="";
+            }else if(s[i] !=' '){
+                temp+=s[i];
+            }
+        }
+        
+        if(temp.length()>0){
+            p.push(temp);
+        }
+        
+        bool notFirst = false;
+        
+        while(!p.empty()){
+            if(notFirst){
+                ans+=' ';
+            }
+           string val = p.top();
+            ans+=val;
+            p.pop();
+            notFirst = true;
+        }
+        
+        return ans;
+    }
+};
